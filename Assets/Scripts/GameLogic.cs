0d28d9f975148d4eb6982c455a6b8beee1001117
Mauno Vähä <maunovaha@gmail.com>
2019-02-02 21:44:12 +0200

    Adds the cooooodes

---
+++ Assets/Scripts/GameLogic.cs
@@ -0,0 +1,41 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using TMPro;
+
+public class GameLogic : MonoBehaviour
+{
+    [SerializeField]
+    private int min = 1;
+
+    [SerializeField]
+    private int max = 1000;
+
+    [SerializeField]
+    private TextMeshProUGUI guessText;
+
+    private int guess;
+
+    public void OnPressHigher()
+    {
+        min = guess;
+        NextGuess();
+    }
+
+    public void OnPressLower()
+    {
+        max = guess;
+        NextGuess();
+    }
+    
+    private void Start()
+    {
+        NextGuess();
+    }
+
+    private void NextGuess()
+    {
+        guess = Random.Range(min, max + 1);
+        guessText.text = guess.ToString();
+    }
+}
